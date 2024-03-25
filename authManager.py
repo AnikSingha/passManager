66608e74ef68d4f9292efc00ee5cd7e440059578
Anik Singha <aniksingha@outlook.com>
2024-03-25 12:20:21 -0400

    start work on AuthManager class

--- authManager.py
+++ authManager.py
@@ -6,12 +6,31 @@ class AuthManager:
     def __init__(self, client):
         self.client = client
 
-    def addUser(self, email, password):
-        
+    def hash_password(self, password):
+        encoded = password.encode('utf-8')
+        hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+        return hashed
+    
+    def check_password(self, password, hash):
+        encoded = password.encode('utf-8')
+        return bcrypt.checkpw(encoded, hash)
+    
+    def add_user(self, email, password):
+        hashed_pass = self.hash_password(password)
+
         try:
-            db = self.client["accounts"]
-        except:
+            db = self.client["passManager"]
+
+            accounts_collection = db["accounts"]
+            password_collection = db["passwords"]
+
+            accounts_collection.insert_one({email : hashed_pass})
+            password_collection.insert_one()
+
+        except Exception as e:
             pass
+
+
             
 
     
