32a951e95fba7799b2133c0dcae3c4404769f91e
Anik Singha <aniksingha@outlook.com>
2024-03-25 11:01:01 -0400

    Add docstrings to the passManager class

--- passManager.py
+++ passManager.py
@@ -1,21 +1,49 @@
-from dbClient import key
 from cryptography.fernet import Fernet
 
 class PassManager:
 
-    def __init__(self, client):
-        self.key = key
-        self.cipher = Fernet(self.key)
+    def __init__(self, client, key):
+        """
+        Initializes a PassManager object with the provided MongoDB client.
+
+        Parameters:
+            client: A MongoDB client object used for storing and retrieving password data.
+            key (bytes): The encryption key used for encrypting the data. It should be a 32-byte Fernet key
+                         generated using Fernet.generate_key().
+        """
+
         self.client = client
+        self.key = key
+        self.cipher = Fernet(self.key) # used for encryption and decryption
     
     def decodePasswords(self, passwords):
+        """
+        Decrypts the encrypted passwords stored in the given dictionary.
+
+        Parameters:
+            passwords (dict): A dictionary where the keys represent website names and the values are encrypted passwords.
+
+        Returns:
+            dict: An updated passwords dictionary where the values are now decrypted.
+        """
         for key, val in passwords.items():
-            decrypted = self.cipher.decrypt(val).decode()
+            decrypted = self.cipher.decrypt(val).decode() # the decrypted password 
             passwords[key] = decrypted
         
         return passwords
     
     def addPassword(self, user, website, password):
+        """
+        Encrypts a password and then stores it in the MongoDB passwords collection.
+
+        Parameters:
+            user (str): The username of the user.
+            website (str): The name of the website.
+            password (str): The unencrypted password we will encrypt and place into the database.
+
+        Returns:
+            bool: A boolean representing whether the function succeeded or failed in updating the database.
+        """
         encryptedPass = self.cipher.encrypt(password.encode('utf-8'))
         operation = {"$set": {f"accounts.{website}": encryptedPass}}
 
@@ -30,6 +58,15 @@ class PassManager:
         return True
     
     def getPasswords(self, userName):
+        """
+        Returns a dictionary containing decrypted passwords for a specific user.
+
+        Parameters:
+            userName (str): The username of the user whose passwords we want to retrieve.
+
+        Returns:
+            dict: A dictionary where the keys are the names of websites and the values are the decrypted passwords.
+        """
         try:
             db = self.client["passManager"]
             user = db.passwords.find_one({"user" : userName})
@@ -38,12 +75,23 @@ class PassManager:
             print(e)
             return False
 
-        return self.decodePasswords(user["accounts"])
+        return self.decodePasswords(user["accounts"]) # decodes the passwords before returning them
 
 
     def updatePassword(self, user, website, password):
+        """
+        Updates the password for an existing website in the database
+
+        Parameters:
+            user (str): The username of the user.
+            website (str): The name of the website.
+            password (str): The unencrypted password we will encrypt and place into the database.
+
+        Returns:
+            bool: A boolean representing whether the function succeeded or failed in updating the database.
+        """
         try:
-            self.addPassword(user, website, password)
+            self.addPassword(user, website, password) # we can simple reuse the addPassword function
 
         except Exception as e:
             print(e)
@@ -52,6 +100,16 @@ class PassManager:
         return True
     
     def deletePassword(self, user, website):
+        """
+        Deletes the credentials for a specific website from the database
+
+        Parameters:
+            user (str): The username of the user.
+            website (str): The name of the website.
+
+        Returns:
+            bool: A boolean representing whether the function succeeded or failed in updating the database.
+        """
         operation = {"$unset" : {f"accounts.{website}" : ""}}
 
         try:
